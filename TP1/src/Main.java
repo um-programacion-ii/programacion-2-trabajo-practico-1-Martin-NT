3e157bce92910800c1e3db31f13b52c0c828fc82
Martin-NT <martinnavarro1503@gmail.com>
2025-04-07 13:19:23 -0300

    ArrayList Vehiculos

--- TP1/src/Main.java
+++ TP1/src/Main.java
@@ -1,17 +1,25 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         VehiculoPrinter printer = new VehiculoPrinter();
 
-        // Crear Vehiculos
+        // Crear Lista de Vehiculos
+        ArrayList<Vehiculo> vehiculos = new ArrayList<Vehiculo>();
+
+        // Crear distintos Vehiculos
         Vehiculo v1 = new Vehiculo("ABC123", "Toyota Hilux SRX", 2019, 2200);
         Vehiculo v2 = new Vehiculo("XYZ678", "Ford Ranger Limited", 2022, 2500);
         Vehiculo v3 = new Vehiculo("JKL321", "Volkswagen Amarok V6", 2021, 2800);
-
-        // Crear Camion
         Camion v4 = new Camion("TRK789", "Mercedes-Benz Actros", 2020, 18000, true);
-
-        // Crear Auto
         Auto v5 = new Auto("LMN456", "Peugeot 207 Compact XS", 2012, 1200, 5);
+
+        // Agregar Vehiculos a la Lista
+        vehiculos.add(v1);
+        vehiculos.add(v2);
+        vehiculos.add(v3);
+        vehiculos.add(v4);
+        vehiculos.add(v5);
 
         // Imprimir todos los vehiculos usando VehiculosPrinter
         printer.imprimir(v1);
