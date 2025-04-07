7095d56a007d24148d8e66da0a370a1b6c77e898
Martin-NT <martinnavarro1503@gmail.com>
2025-04-07 11:27:12 -0300

    VehiculoPrinter con Camion

--- TP1/src/VehiculoPrinter.java
+++ TP1/src/VehiculoPrinter.java
@@ -6,6 +6,19 @@ public class VehiculoPrinter {
         System.out.println("--> Marca: " + vehiculo.getMarca());
         System.out.println("--> Año: " + vehiculo.getAnio());
         System.out.println("--> Capacidad de carga (Kg): " + vehiculo.getCapacidadCargaKg());
+
+        // Solo si es un Camion, imprimimos si tiene acoplado (Tradicional)
+        if (vehiculo instanceof Camion) {
+            Camion camion = (Camion) vehiculo;
+            System.out.println("--> Tiene Acoplado: " + camion.isTieneAcoplado());
+        }
+
+        /* Pattern Matching (desde Java 16 en adelante)
+        if (vehiculo instanceof Camion camion) {
+            System.out.println("--> Tiene acoplado: " + camion.isTieneAcoplado());
+        }
+        */
+        
         System.out.println("-----------------------------------------------------------------------------");
     }
 }
